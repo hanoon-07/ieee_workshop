4bd2c586695af521caf25af993855a0685127c24
hanoon-07 <muhhamedhanoon@gmail.com>
2025-09-13 01:46:43 +0530

    react proj

---
+++ bg_change/my-app/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="w-full h-screen duration-200"
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "red"}}
+          >Red</button>
+          <button
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "green"}}
+          >Green</button>
+          <button
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "blue"}}
+          >Blue</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
