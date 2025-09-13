5997444993418e7c598224543177a93b042f6f84
hanoon-07 <muhhamedhanoon@gmail.com>
2025-09-13 09:31:24 +0530

    changes

--- react_proj/my-app/src/App.jsx
+++ react_proj/my-app/src/App.jsx
@@ -8,50 +8,45 @@ import { useState, useCallback, useEffect, useRef } from 'react'
 function App() {
     // create state for length, numberallowed, charallowed, pass
 
-    const [length, setLength] = useState(6)
-    const [numberAllowed,setNumberAllowed] = useState(false)
-    const [charAllowed, setCharAllowed] = useState(false)
-    const [password, setPassword] = useState("")
+
 
 
     //useRef hook to be added
 
-    const input_ref = useRef(null)
+  
 
 
-    const passwordGenerator = useCallback(() => {
-        let pass = ""
-        let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-        if (numberAllowed) str += "0123456789"
-        if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
+    // const passwordGenerator = useCallback(() => {
+    //     let pass = ""
+    //     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    //     if (numberAllowed) str += "0123456789"
+    //     if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
 
-        for (let i = 1; i <= length; i++) {
-            let char = Math.floor(Math.random() * str.length);
-            pass += str.charAt(char)
+    //     for (let i = 1; i <= length; i++) {
+    //         let char = Math.floor(Math.random() * str.length);
+    //         pass += str.charAt(char)
 
-        }
+    //     }
 
-        setPassword(pass)
+    //     setPassword(pass)
 
-    }, [length, numberAllowed, charAllowed])
+    // }, [length, numberAllowed, charAllowed])
 
 
 
-    const copyPasswordToClipboard = useCallback(() => {
-        input_ref.current?.select();
-        input_ref.current?.setSelectionRange(0, 999);
+    // const copyPasswordToClipboard = useCallback(() => {
+    //     input_ref.current?.select();
+    //     input_ref.current?.setSelectionRange(0, 999);
 
-        window.navigator.clipboard.writeText(password)
-    }, [password])
+    //     window.navigator.clipboard.writeText(password)
+    // }, [password])
 
 
 
 
     //useEffect for 4 dependencies
 
-    useEffect(() => {
-        passwordGenerator()
-    }, [length, charAllowed, numberAllowed])
+    
     
 
 
@@ -65,8 +60,7 @@ function App() {
                     type="text"
                     className="outline-none w-full py-1 px-3"
                     placeholder="Password"
-                    value={password}
-                    ref={input_ref}
+                    
                     readOnly
                     //value
                     //add ref
@@ -74,7 +68,7 @@ function App() {
                 />
                 <button
                     //add onClick function
-                    onClick={copyPasswordToClipboard}
+                    
 
                     className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
                 >copy</button>
@@ -87,15 +81,14 @@ function App() {
                         min={6}
                         max={100}
                         className='cursor-pointer'
-                        onChange={(e) => { setLength(e.currentTarget.value) }}
-                        value={length}
+                        
                         
 
                     // value and onChange to be added
 
                     />
                     {/* length need to be added as state */}
-                    <label>Length: {length}</label>
+                    <label>Length: </label>
 
                 </div>
                 <div className="flex items-center gap-x-1">
@@ -106,7 +99,7 @@ function App() {
 
                         //onChange set prev to !prev
 
-                        onChange={(e)=>{setNumberAllowed(prev=>!prev)}}
+                        
 
                     />
                     <label htmlFor="numberInput">Numbers</label>
@@ -118,7 +111,7 @@ function App() {
                         //defaultChecked={charAllowed}
                         //onChange set
 
-                        onChange={(e)=>{setCharAllowed(prev=>!prev)}}
+                        
 
                     />
                     <label htmlFor="characterInput">Characters</label>
@@ -130,4 +123,3 @@ function App() {
 }
 
 export default App
-
