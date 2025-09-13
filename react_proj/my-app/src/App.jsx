0634a4fd604c094fc932c5921040af3a6670684a
hanoon-07 <muhhamedhanoon@gmail.com>
2025-09-13 07:58:24 +0530

    notes

--- react_proj/my-app/src/App.jsx
+++ react_proj/my-app/src/App.jsx
@@ -8,37 +8,51 @@ import { useState, useCallback, useEffect, useRef } from 'react'
 function App() {
     // create state for length, numberallowed, charallowed, pass
 
-   
+    const [length, setLength] = useState(6)
+    const [numberAllowed,setNumberAllowed] = useState(false)
+    const [charAllowed, setCharAllowed] = useState(false)
+    const [password, setPassword] = useState("")
 
 
     //useRef hook to be added
 
-
-    // const passwordGenerator = useCallback(() => {
-    //     let pass = ""
-    //     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    //     if (numberAllowed) str += "0123456789"
-    //     if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
-
-    //     for (let i = 1; i <= length; i++) {
-    //         let char = Math.floor(Math.random() * str.length);
-    //         pass += str.charAt(char)
-
-    //     }
-
-    //     setPassword(pass)
+    const input_ref = useRef(null)
 
 
-    // }, [length, numberAllowed, charAllowed, setPassword])
+    const passwordGenerator = useCallback(() => {
+        let pass = ""
+        let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        if (numberAllowed) str += "0123456789"
+        if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
 
-    // const copyPasswordToClipboard = useCallback(() => {
-    //     passwordRef.current?.select();
-    //     passwordRef.current?.setSelectionRange(0, 999);
-    //     window.navigator.clipboard.writeText(password)
-    // }, [password])
+        for (let i = 1; i <= length; i++) {
+            let char = Math.floor(Math.random() * str.length);
+            pass += str.charAt(char)
+
+        }
+
+        setPassword(pass)
+
+    }, [length, numberAllowed, charAllowed])
+
+
+
+    const copyPasswordToClipboard = useCallback(() => {
+        input_ref.current?.select();
+        input_ref.current?.setSelectionRange(0, 999);
+
+        window.navigator.clipboard.writeText(password)
+    }, [password])
+
+
 
 
     //useEffect for 4 dependencies
+
+    useEffect(() => {
+        passwordGenerator()
+    }, [length, charAllowed, numberAllowed])
+    
 
 
 
@@ -51,6 +65,8 @@ function App() {
                     type="text"
                     className="outline-none w-full py-1 px-3"
                     placeholder="Password"
+                    value={password}
+                    ref={input_ref}
                     readOnly
                     //value
                     //add ref
@@ -58,6 +74,7 @@ function App() {
                 />
                 <button
                     //add onClick function
+                    onClick={copyPasswordToClipboard}
 
                     className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
                 >copy</button>
@@ -70,22 +87,26 @@ function App() {
                         min={6}
                         max={100}
                         className='cursor-pointer'
+                        onChange={(e) => { setLength(e.currentTarget.value) }}
+                        value={length}
                         
 
                     // value and onChange to be added
 
                     />
                     {/* length need to be added as state */}
-
-                    <label>Length: </label>
+                    <label>Length: {length}</label>
 
                 </div>
                 <div className="flex items-center gap-x-1">
                     <input
                         type="checkbox"
                         id="numberInput"
-                        //defaultChecked={numberAllowed}
+                        // defaultChecked={numberAllowed}
+
                         //onChange set prev to !prev
+
+                        onChange={(e)=>{setNumberAllowed(prev=>!prev)}}
 
                     />
                     <label htmlFor="numberInput">Numbers</label>
@@ -96,6 +117,8 @@ function App() {
                         id="characterInput"
                         //defaultChecked={charAllowed}
                         //onChange set
+
+                        onChange={(e)=>{setCharAllowed(prev=>!prev)}}
 
                     />
                     <label htmlFor="characterInput">Characters</label>
